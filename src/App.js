9cda2f7f685ccf2e76372495bc7c51087fa91d74
Aisha <suleamaaisha@yahoo.com>
2019-08-26 23:56:20 +0200

    provides food recipes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Form from "./component/Form";
+import Recipes from "./component/Recipes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = '5952d427a1178bff343b7b3f751166f8'
+
+class App extends Component {
+  state ={
+    recipes: []
+  }
+  getRecipe= async (e) => {
+    const recipeName = e.target.elements.recipeName.value
+
+    e.preventDefault()
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`)
+
+    const data = await api_call.json()
+    this.setState({recipes: data.recipes})
+  }
+  componentDidMount() {
+    const json = localStorage.getItem('recipes')
+    const recipes = JSON.parse(json)
+    this.setState({recipes: recipes})
+  }
+
+  componentDidUpdate() {
+    const recipes = JSON.stringify(this.state.recipes)
+    localStorage.setItem('recipes', recipes)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe}/>
+        <Recipes recipes={this.state.recipes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
